6b42a1add67cf71ef33ebd690ef35cb31b3ae5ca
xiyuanzhou <99446193+xiyuanzhou@users.noreply.github.com>
2022-03-08 21:00:22 -0800

    change2

--- timeit.py
+++ timeit.py
@@ -3,16 +3,16 @@ import time
 
 def calculate_time(func):
     def wrapper():
-        
         start = time.time()
+        func()
         end = time.time()
-        
-        print (f'Total time: {func(end - start)}')
+        print(f'Total time: {end - start}')
     return wrapper
 
+
 @calculate_time
-def func(times):
-    return times
+def func():
+    return 0
 
 
 
