07a73685211c0a175f3997e0bca80f58cb0faf8e
xiyuanzhou <99446193+xiyuanzhou@users.noreply.github.com>
2022-03-10 01:33:25 -0800

    Add files via upload

---
+++ document_analyzer.py
@@ -0,0 +1,69 @@
+
+
+
+
+myfile = open('document.txt', 'r')
+
+#print(myfile.read())
+
+words = myfile.read()
+
+#print(words)
+
+count_words = words.split()
+
+
+cout_word = {}
+
+#for i in range(len(count_words)):
+    #count_words[i] = count_words[i].lower() 
+
+for n in count_words:
+    if  n not in cout_word.keys():
+        cout_word[n] = 1
+    else:
+        cout_word[n] += 1
+
+mylist = []
+
+for j in cout_word.items():
+    mylist.append(j)
+
+for items in range(len(mylist)):
+    
+    for k in range(0,(len(mylist)- items - 1)):
+        if mylist[k][1] < mylist[k+1][1]:
+            mylist[k], mylist[k+1] = mylist[k+1], mylist[k]
+
+
+            
+print(mylist)
+final_temp = []
+
+for index in range(0,5):
+    final_temp.append(mylist[index])
+
+for i in range(len(final_temp)):
+    for k in range(0,(len(final_temp)- i - 1)):
+        if final_temp[k][0] > final_temp[k+1][0]:
+            final_temp[k], final_temp[k+1] = final_temp[k+1], final_temp[k]
+
+print(final_temp)
+    
+    #print(f'{mylist[index][0]}: {mylist[index][1]}')
+    
+#sort_value = sorted(cout_word.values())
+
+
+#for key,value in cout_word.items():
+
+#    print(f'{key}: {value}')
+    
+
+
+#print(sort_value)
+
+
+myfile.close()
+    
+ 
