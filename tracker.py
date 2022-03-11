c9c1bd0ea235564d271e09c51d34331fb22a1adf
xiyuanzhou <99446193+xiyuanzhou@users.noreply.github.com>
2022-03-10 21:21:43 -0800

    final fixed

--- tracker.py
+++ tracker.py
@@ -1,5 +1,8 @@
 def func_counter(func):
     def wrapper():
+        
+        #keep track on how many time being call
+        #add one each time
         wrapper.counter += 1
         return func
     wrapper.counter = 0
@@ -11,9 +14,9 @@ def func_counter(func):
 def foo():
     return 2**3-34
 
-
-foo()
-foo()
-print(foo.counter) # expect 2 as output
+#code by Xiyuan
+#foo()
+#foo()
+#print(foo.counter) # expect 2 as output
     
     
