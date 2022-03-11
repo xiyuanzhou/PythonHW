cee43b20c9a3b87a96eb635ebcfee2a52980bcac
xiyuanzhou <99446193+xiyuanzhou@users.noreply.github.com>
2022-03-10 21:23:39 -0800

    final fixed

--- timeit.py
+++ timeit.py
@@ -3,10 +3,18 @@ import time
 
 def calculate_time(func):
     def wrapper():
+        
+        #initial starting time 
         begin_time = time.time()
         func()
+        
+        #time end here 
         finish_run = time.time()
+        
+        #final running time for the function called
         runtime = finish_run - begin_time
+        
+        #print result
         print(f'Total time {runtime}')
         #return value
         
@@ -18,5 +26,5 @@ def calculate_time(func):
 def func():
     time.time()
 
-
+#code by Xiyuan Zhou
 
