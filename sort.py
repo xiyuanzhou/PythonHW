093eb43f595825e69b888a1228a67f35e648aca3
xiyuanzhou <99446193+xiyuanzhou@users.noreply.github.com>
2022-03-10 21:26:33 -0800

    final fixed

--- sort.py
+++ sort.py
@@ -1,15 +1,27 @@
 def sort_list(mylist):
+    
+    #get the size/length of mylist list 
     n = len(mylist)
     i = 0
+    
+    #start the first loop for the whole 
     while i < n - 1:
         k = 0
+        
+        #checking the rest loop index
+        #excpet for the iter index
         while n - i - 1 > k:
             
+            #compare the value
+            #if bigger then swap
             if mylist[k] > mylist[k + 1]:
                 mylist[k], mylist[k + 1] = mylist[k + 1], mylist[k]
             
             k = k + 1
                 
-        i = i + 1
+        i = i + 1 
         
     return mylist
+
+
+#code by Xiyuan Zhou
